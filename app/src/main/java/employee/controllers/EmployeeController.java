58332af9d1336731481a9a0907462cc54ebc9bdb
khoahoang <khoahoang@kms-technology.com>
2015-08-25 14:05:04 +0700

    Fix bootstrap and style.css

--- app/src/main/java/employee/controllers/EmployeeController.java
+++ app/src/main/java/employee/controllers/EmployeeController.java
@@ -1,15 +1,16 @@
 package employee.controllers;
 
-import java.util.Set;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import employee.domain.Employee;
 import employee.services.EmployeeService;
+import employee.services.viewmodels.EmployeeModel;
 
 @RestController
 @RequestMapping("/employees")
@@ -17,10 +18,10 @@ public class EmployeeController {
 	
 	@Autowired
 	private EmployeeService service;
-	
+//	/.stream().map(e-> new EmployeeModel(e.getId(), e.getName(), e.getAge(),e.getGender() , e.getCountry().getId()))
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<Set<Employee>> gets(){
-		
+	public ResponseEntity<List<EmployeeModel>> gets(){
+		return new ResponseEntity<List<EmployeeModel>>(service.gets(), HttpStatus.ACCEPTED);
 	}
 	
 	
--- app/src/main/java/employee/services/EmployeeService.java
+++ app/src/main/java/employee/services/EmployeeService.java
@@ -1,11 +1,12 @@
 package employee.services;
 
+import java.util.List;
 import java.util.Set;
 
 import employee.services.viewmodels.EmployeeModel;
 
 public interface EmployeeService {
-	Set<EmployeeModel> gets();
+	List<EmployeeModel> gets();
 	EmployeeModel add(EmployeeModel employee);
 	EmployeeModel update(EmployeeModel employee);
 	void delete(long id);
